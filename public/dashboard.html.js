1e671c2c353e9e11d0af9a49149636d0b92896de
J10a1n15 <jan.leger@web.de>
2023-01-24 19:54:11 +0100

    Redirect to homepage if not logged in

--- public/dashboard.html.js
+++ public/dashboard.html.js
@@ -1,8 +1,14 @@
 window.onload = function () {
     fetch('/get_extra_data')
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
+        //.then(response => response.json())
+        .then(async data => {
+            try {
+                data = await data.json();
+            } catch (err) {
+                console.error(err);
+                if (!alert("You are not logged in. Please login to continue.")) return document.location = '/';
+            }
+
             const h1 = document.createElement('h1');
             h1.appendChild(document.createTextNode(`Hello, ${data.displayName} (${data.name})!`));
             document.body.appendChild(h1);
